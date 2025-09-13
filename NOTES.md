6ae0cf07865278fb13ab757f6030345d80a1a487
August-Light <mxy030223@126.com>
2025-09-14 00:04:28 +0800

    Initial commit

---
+++ NOTES.md
@@ -0,0 +1,29 @@
+# Kotlin
+
+Tutorial：
+
+https://book.kotlincn.net/text/kotlin-tour-welcome.html
+
+在线 IDE：
+
+https://play.kotlinlang.org/
+
+# 用到的 JS 库
+
+https://www.vexflow.com/
+
+https://github.com/0xfe/vexflow
+
+https://github.com/0xfe/vexflow/wiki/Using-EasyScore
+
+---
+
+https://tonejs.github.io/
+
+---
+
+ChatGPT 给了一个库叫做 OSMD：
+
+https://github.com/opensheetmusicdisplay/opensheetmusicdisplay
+
+ChatGPT 声称 OSMD 比 Vexflow 更适合做乐谱显示，似乎有针对 MusicXML 的功能。
---
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -0,0 +1,58 @@
+package com.example.helloworld
+
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.helloworld.ui.theme.HelloWorldTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            HelloWorldTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+
+        val webView = WebView(this)
+        setContentView(webView)
+
+        webView.settings.javaScriptEnabled = true
+        webView.webViewClient = WebViewClient() // 防止跳转到外部浏览器
+
+        // 加载 assets 下的 index.html
+        webView.loadUrl("file:///android_asset/index.html")
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    HelloWorldTheme {
+        Greeting("Android")
+    }
+}
